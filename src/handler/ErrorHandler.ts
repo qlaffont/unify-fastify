ceb15786fd3b4be964815e8b1bd7279a96540fbc
mike <mike@flexper.fr>
2022-05-24 15:33:32 +0200

    feat: all unified errors in handler + associated routes

--- src/handler/ErrorHandler.ts
+++ src/handler/ErrorHandler.ts
@@ -1,9 +1,54 @@
 import { FastifyError, FastifyRequest, FastifyReply } from 'fastify'
+import { CustomError, BadRequest, Unauthorized, Forbidden, NotFound, RequestTimeOut, InternalServerError, NotImplemented } from 'unify-errors'
 
 const errorHandler = (error: FastifyError, request: FastifyRequest, reply: FastifyReply) => {
-  console.error('error', error)
+  if(error instanceof CustomError) {
+    let httpCode: number=0, message: any = {}
 
-  reply.status(409).send({ ok: false })
+    switch(error) {
+      case BadRequest(): {
+        httpCode=400
+        message={msg: "That's bad request"}
+        break;
+      }
+      case Unauthorized(): {
+        httpCode=401
+        message={msg: "That's unauthorized"}
+        break;
+      }
+      case Forbidden(): {
+        httpCode=403
+        message={msg: "That's forbidden"}
+        break;
+      }
+      case NotFound(): {
+        httpCode=404
+        message={msg: "That's not found"}
+        break;
+      }
+      case RequestTimeOut(): {
+        httpCode=408
+        message={msg: "That's time out"}
+        break;
+      }
+      case InternalServerError(): {
+        httpCode=500
+        message={msg: "That's intrenal server error"}
+        break;
+      }
+      case NotImplemented(): {
+        httpCode=501
+        message={msg: "That's not implemented"}
+        break;
+      }
+    }
+
+    console.error('Custom error handled', error)
+    reply.status(httpCode).send(message)
+  }
+
+  console.error('Unhandled error', error)
+  reply.status(500).send({ error: 'An unexpected error occured'})
 }
 
 export default errorHandler
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import fastify from 'fastify'
-import { BadRequest } from 'unify-errors';
+import { BadRequest, Forbidden, Unauthorized } from 'unify-errors';
 import errorHandler from './handler/ErrorHandler';
 
 ////////////////////////////////////////
@@ -17,6 +17,34 @@ server.get('/ping', async (request, reply) => {
 
 server.get('/bad-request', async (request, reply) => {
   throw BadRequest({ example: 'A bad request error'})
+})
+
+server.get('/unauthorized', async (request, reply) => {
+  throw Unauthorized({ example: 'An unauthorized error'})
+})
+
+server.get('/forbidden', async (request, reply) => {
+  throw Forbidden({ example: 'A forbidden error'})
+})
+
+server.get('/not-found', async (request, reply) => {
+  throw BadRequest({ example: 'A not found error'})
+})
+
+server.get('/request-time-out', async (request, reply) => {
+  throw BadRequest({ example: 'A request time out error'})
+})
+
+server.get('/internal', async (request, reply) => {
+  throw BadRequest({ example: 'An internal server error'})
+})
+
+server.get('/not-implemented', async (request, reply) => {
+  throw BadRequest({ example: 'A not implemented error'})
+})
+
+server.get('/not-custom', async (request, reply) => {
+  throw new Error('A generic, not customized error')
 })
 
 ////////////////////////////////////////
