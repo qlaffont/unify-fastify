1db2936c4626bbeadb6aaa17e1fce6251fcfa069
mike <mike@flexper.fr>
2022-07-04 15:56:55 +0200

    feat(test): full test

--- src/handler/ErrorHandler.ts
+++ src/handler/ErrorHandler.ts
@@ -3,55 +3,46 @@ import { CustomError, BadRequest, Unauthorized, Forbidden, NotFound, TimeOut, In
 
 const errorHandler = (error: FastifyError, _: FastifyRequest, reply: FastifyReply) => {
   if(error instanceof CustomError) {
-    let httpCode: number=0, message: any = {}
+    const response = (error as CustomError).context || '';
+    let httpCode: number = 0;
 
     switch(error.constructor) {
       case BadRequest: {
         httpCode=400
-        message={msg: "That's bad request"}
         break;
       }
       case Unauthorized: {
         httpCode=401
-        message={msg: "That's unauthorized"}
         break;
       }
       case Forbidden: {
         httpCode=403
-        message={msg: "That's forbidden"}
         break;
       }
       case NotFound: {
         httpCode=404
-        message={msg: "That's not found"}
         break;
       }
       case TimeOut: {
         httpCode=408
-        message={msg: "That's time out"}
         break;
       }
       case InternalServerError: {
         httpCode=500
-        message={msg: "That's intrenal server error"}
         break;
       }
       case NotImplemented: {
         httpCode=501
-        message={msg: "That's not implemented"}
         break;
       }
       default: {
         httpCode=500
-        message={msg: "Custom error not handled"}
         break;
       }
     }
 
-    console.error('Custom error handled: ', error)
-    reply.status(httpCode).send(message)
+    reply.status(httpCode).send(response)
   } else {
-    console.error('Unhandled error', error)
     reply.status(500).send({ error: 'An unexpected error occured'})
   }
 }
--- test/errrPlugin.test.ts
+++ test/errrPlugin.test.ts
@@ -5,13 +5,13 @@ import makeServer from './core/Server';
 
 let server: FastifyInstance
 
-const testRoute = async (server: FastifyInstance, routePath: string, supposedMessage: string, supposedStatus: number) => {
+const testRoute = async (server: FastifyInstance, routePath: string, supposedMessage: Record<string, unknown>, supposedStatus: number) => {
   const response = await server.inject({
     method: 'GET',
     url: routePath
   })
 
-  expect(JSON.parse(response.body).msg).toBe(supposedMessage);
+  expect(JSON.parse(response.body)).toStrictEqual(supposedMessage);
   expect(response.statusCode).toBe(supposedStatus);
 }
 
@@ -23,6 +23,66 @@ beforeEach(() => {
 
 describe('errorPlugin', () => {
   it('bad request', async () => {
-    await testRoute(server, '/bad-request', "That's bad request", 400)
+    await testRoute(server,
+      '/bad-request',
+      { example: 'A bad request error'},
+      400
+    )
+  });
+
+  it('unauthorized', async () => {
+    await testRoute(server,
+      '/unauthorized',
+      { example: 'An unauthorized error'},
+      401
+    )
+  });
+
+  it('forbidden', async () => {
+    await testRoute(server,
+      '/forbidden',
+      { example: 'A forbidden error'},
+      403
+    )
+  });
+
+  it('not-found', async () => {
+    await testRoute(server,
+      '/not-found',
+      { example: 'A not found error'},
+      404
+    )
+  });
+
+  it('time-out', async () => {
+    await testRoute(server,
+      '/request-time-out',
+      { example: 'A request time out error'},
+      408
+    )
+  });
+
+  it('internal', async () => {
+    await testRoute(server,
+      '/internal',
+      { example: 'An internal server error'},
+      500
+    )
+  });
+
+  it('not-implemented', async () => {
+    await testRoute(server,
+      '/not-implemented',
+      { example: 'A not implemented error'},
+      501
+    )
+  });
+
+  it("error not extending 'CustomError' from 'unify-errors'", async () => {
+    await testRoute(server,
+      '/not-custom',
+      { error: 'An unexpected error occured'},
+      500
+    )
   });
 });
