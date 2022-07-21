beeaa315e758197897a4a320b950b3721839d01d
mike-flexper <104772482+mike-flexper@users.noreply.github.com>
2022-07-21 15:51:40 +0200

    feat(response): add error name (#12)
    * chore: bump up
    
    * feat(response): include error name
    
    * feat(test): updated
    
    * fix(pkg): update pre commit hook
    
    * fix(github): only run 16
    
    * fix(runner): remove useless bug with windows
    
    Co-authored-by: Quentin Laffont <quentin@flexper.fr>

--- example/server.ts
+++ example/server.ts
@@ -1,62 +1,71 @@
-import fastify from 'fastify'
-import { BadRequest, Forbidden, Unauthorized, InternalServerError, NotFound, NotImplemented, TimeOut } from 'unify-errors';
-import unifyFastify from 'unify-fastify';
+import fastify from 'fastify';
+import {
+  BadRequest,
+  Forbidden,
+  InternalServerError,
+  NotFound,
+  NotImplemented,
+  TimeOut,
+  Unauthorized,
+} from 'unify-errors';
+
+import unifyFastify from '../src';
 
 ////////////////////////////////////////
 // Setup
 
-const server = fastify()
-server.register(unifyFastify )
+const server = fastify();
+server.register(unifyFastify);
 
 server.get('/ping', async () => {
-  return 'pong\n'
-})
+  return 'pong\n';
+});
 
 ////////////////////////////////////////
 // Test error routes
 
 server.get('/bad-request', async () => {
-  throw new BadRequest({ example: 'A bad request error'})
-})
+  throw new BadRequest({ example: 'A bad request error' });
+});
 
 server.get('/unauthorized', async () => {
-  throw new Unauthorized({ example: 'An unauthorized error'})
-})
+  throw new Unauthorized({ example: 'An unauthorized error' });
+});
 
 server.get('/forbidden', async () => {
-  throw new Forbidden({ example: 'A forbidden error'})
-})
+  throw new Forbidden({ example: 'A forbidden error' });
+});
 
 server.get('/not-found', async () => {
-  throw new NotFound({ example: 'A not found error'})
-})
+  throw new NotFound({ example: 'A not found error' });
+});
 
 server.get('/request-time-out', async () => {
-  throw new TimeOut({ example: 'A request time out error'})
-})
+  throw new TimeOut({ example: 'A request time out error' });
+});
 
 server.get('/internal', async () => {
-  throw new InternalServerError({ example: 'An internal server error'})
-})
+  throw new InternalServerError({ example: 'An internal server error' });
+});
 
 server.get('/not-implemented', async () => {
-  throw new NotImplemented({ example: 'A not implemented error'})
-})
+  throw new NotImplemented({ example: 'A not implemented error' });
+});
 
 server.get('/not-custom', async () => {
-  throw new Error('A generic, not customized error')
-})
+  throw new Error('A generic, not customized error');
+});
 
 ////////////////////////////////////////
 
 const config = {
   port: 8080,
-}
+};
 
 server.listen(config, (err, address) => {
   if (err) {
-    console.error(err)
-    process.exit(1)
+    console.error(err);
+    process.exit(1);
   }
-  console.log(`Server listening at ${address}`)
-})
+  console.log(`Server listening at ${address}`);
+});
